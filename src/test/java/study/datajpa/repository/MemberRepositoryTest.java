d3227bbf154927c48ee293797cf42442d3d301ac
taek <tttck88@gmail.com>
2023-01-30 17:46:46 +0900

    스프링 데이터 jpa 환경설정 및 도메인 모델 세팅

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void testMember() {
+        // given
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).orElseGet(null);
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+
+        // when
+
+        // then
+    }
+
+}
